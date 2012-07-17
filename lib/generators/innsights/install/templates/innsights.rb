f9e18105006ffe643a42cfcefa0c515d2db9370e
Rogelio Guzman <chinog9@gmail.com>
2012-07-17 12:00:21 -0500

    Added :config and :on block documentations to innsights.rb template

--- lib/generators/innsights/install/templates/innsights.rb
+++ lib/generators/innsights/install/templates/innsights.rb
@@ -38,4 +38,35 @@ Innsights.setup do
   #   report :registration
   # end
   
+  ## Controller actions can also repost to Innsights
+  
+  # Params:
+  #   "controller#action": Speficies the controller and actions when it should report to innsights
+  #
+  # Options:
+  #   is_user(Detault false): Set to true if it will act directly on a user 
+  #
+  # on 'user#new' do
+  #   report 'New User'
+  #   user   :current_user
+  #   is_user true
+  # end
+
+
+  ## Configuration can be done inside a config block
+
+  # Params:
+  #   envs: Specify the config options for specific enviroments(:development, :test, :production). 
+  #         If no options env is pass, it will take effect on any enviroment
+  #
+  # Options: 
+  #   queue (Default nil): Speficies a queue_system(:resque, :delayed_job) for reporting to Innsights
+  #   test  (Default false): Used for setting the enviroment on test mode
+  #     
+  
+  # config :development do
+  #   queue :resque
+  #   test  true
+  # end
+
 end
