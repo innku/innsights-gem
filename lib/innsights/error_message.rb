d8a4da5e6d076c26f1f7d08220a8630202fecddf
Rogelio Guzman <chinog9@gmail.com>
2012-08-20 11:16:54 -0500

    Actions log into Rails Story[34598501]

--- lib/innsights/error_message.rb
+++ lib/innsights/error_message.rb
@@ -1,26 +1,39 @@
 module Innsights
   class ErrorMessage
-    
+
     DEFAULT = "Sorry, we are currently having an error. We are moving to get this fixed"
-    
+
     def self.log(error="")
       if Innsights.log_errors
-        return out(debug_message(error)) if error.is_a?(Exception)
-        return out(error) if !error.empty?
-        out(DEFAULT)
+        if error.is_a?(Exception)
+          message = debug_message(error) 
+        elsif !error.empty?
+          message = error
+        else
+          message = DEFAULT
+        end
+        log_into_rails(message)
+        out(message)
       end
     end
-    
+
+
     def self.debug_message(e)
       return "[Innsights Debugging] " << e.message if Innsights.debugging
       return DEFAULT
     end
-    
+
     private
-    
+
+    def self.log_into_rails(message)
+      if Innsights.rails? && Rails.logger
+        Rails.logger.info "[Innsights Debugging ] #{message}"
+      end
+    end
+
     def self.out(msg)
       puts "[Innsights Error] #{msg}"
     end
-    
+
   end
 end
--- spec/innsights/error_message_spec.rb
+++ spec/innsights/error_message_spec.rb
@@ -1,7 +1,53 @@
 require 'spec_helper'
 
 describe Innsights::ErrorMessage do
-  describe '.display_error' do
+  describe '.log' do
+    before do 
+      Innsights.stub!(:log_errors){true}
+      Rails.stub!(:logger){ false }
+    end
+    it 'Outputs the debug_message when there is an exception' do
+      e = Exception.new
+      Innsights::ErrorMessage.should_receive(:debug_message).with(e)
+      Innsights::ErrorMessage.should_receive(:out)
+      Innsights::ErrorMessage.log(e)
+    end
+    it 'Outputs the error When it is not an exception' do
+      e = "some error"
+      Innsights::ErrorMessage.should_receive(:out).with(e)
+      Innsights::ErrorMessage.log(e)
+    end
+    it 'Outputs the default message as the default option' do
+      default = Innsights::ErrorMessage::DEFAULT
+      Innsights::ErrorMessage.should_receive(:out).with(default)
+      Innsights::ErrorMessage.log
+    end
+    describe 'Rails logger' do
+      context 'When Rails is defined and the logger exists' do
+        before do 
+          Innsights.stub!(:rails){true}
+          Rails.stub!(:logger){true}
+        end
+        it 'Logs the error' do
+          Rails.stub_chain(:logger, :info)
+          Rails.logger.should_receive(:info)
+          Innsights::ErrorMessage.log
+        end
+      end
+      context 'When Rails is not defined or the logger does not exist' do
+        before do 
+          Innsights.stub!(:rails){false}
+          Rails.stub!(:logger){false}
+        end
+        it 'Logs the error' do
+          Rails.stub_chain(:logger, :info)
+          Rails.logger.should_not_receive(:info)
+          Innsights::ErrorMessage.log
+        end
+      end
+    end
+  end
+  describe '.debug_message' do
     let(:e) { Exception.new('Error Msg') }
     context 'When debugging is on' do
       before { Innsights.stub(:debugging).and_return(true) }
